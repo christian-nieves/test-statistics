2e8af6e6494f4c250bdd1224e37891d35b8ea58c
christian-nieves <thechristiannieves@gmail.com>
2026-04-16 15:08:22 -0400

    Test Scores

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
 public class TestStatistics {
+    public static void main(String[] args) {
+
+        int[] testScores = {100, 73, 81, 92, 65, 99, 70, 83, 74, 95};
+
+
+    }
 }
